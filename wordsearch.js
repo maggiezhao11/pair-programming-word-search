c7709878cdfa7e4093ecc8c702bad1a7a1eed208
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-30 22:13:33 +0000

    W2D3 pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transposition(letters).map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+    for (k of verticalJoin) {
+      if (k.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  const transposition = function(matrix) {
+    let result = [];
+    let newArray = [];
+    //let row - i - matrix.length
+    //let columns - j - matrix[0].length
+    if (matrix === []) {
+        throw new Error('name is empty');
+    }
+    for (let column = 0; column < matrix[0].length; column++) {
+      for (let row = 0; row < matrix.length; row++) {
+        newArray.push(matrix[row][column]);
+      }
+      result.push(newArray);
+    }
+    
+    return result;
+  };
+    
+  module.exports = wordSearch;
+  
